1e39c10b52a3be02dda5e5da1ab9b1f1043b910f
Øistein Haugland <haugland.1193@gmail.com>
2017-02-26 13:50:55 +0100

    finished the project

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,25 @@
 
-
-
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, dictionary)
-	
+	substrings_in_dictionary = Hash.new(0)
+
+	temp = ""
+	letters = word.split("")
+
+
+	letters.each_with_index do |a,i|
+		
+		letters.each_with_index do |b, t|
+
+		temp = letters[i..t].join("")	
+			if dictionary.include? temp.downcase
+				substrings_in_dictionary[temp] += 1
+			end
+		end
+	end
+
+	return substrings_in_dictionary
 end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
